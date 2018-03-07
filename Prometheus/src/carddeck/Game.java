75e04632edef2d874ca38ff2aa1805a1eb269198
Zinko Victor <zinkoexp@gmail.com>
2018-03-07 15:51:25 +0200

    add new file Game.java

---
+++ Prometheus/src/carddeck/Game.java
@@ -0,0 +1,15 @@
+package carddesk;
+
+public class Game {
+
+	public static void main(String[] args) {
+		Desk game = new Desk();
+		game.order();
+		game.shuffle();
+		System.out.println(game.drawOne().getSuit().getName());		
+	
+		
+
+	}
+
+}
