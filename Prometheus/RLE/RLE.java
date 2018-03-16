74a7a96e6ebd80276d08e0811b0823fc1fdb68a6
Zinko Victor <zinkoexp@gmail.com>
2018-03-16 20:13:22 +0200

    final version RLE

--- Prometheus/RLE/RLE.java
+++ Prometheus/RLE/RLE.java
@@ -2,33 +2,40 @@ package RLE;
 
 public class RLE {	
 	//static StringBuilder print;
-	public static StringBuilder RLEPrint(String str) {
-		StringBuilder string = new StringBuilder(str);
-		StringBuilder toLowerString = new StringBuilder(string);		
+public static void main(String[] args) {
+	
+	if (args == null || args[0].isEmpty()) {
+		System.out.println("");
+	} else {
+		StringBuilder string = new StringBuilder(args[0].toString());
 		StringBuilder temp= new StringBuilder("");
-		string.append(" ");
-		int count = 1;
-		int next = 0;		
+		string.append(' ');
+		int count = 0;
+		int next = 0;
+		
 		for(int i=0;i<string.length()-1;i++) {		
 			if (string.charAt(i)==string.charAt(i+1)) {			
 				count++;				
-				if(count==9) {					
-					temp.append(count);
+				if(count==9) {	
 					temp.append(string.charAt(i));
-					count=1;								
-				}				
-			}
-			else {
-				temp.append(count);
+					temp.append(count);					
+					count=0;								
+				}
+				
+			}			
+			else {		
+				count++;
 				temp.append(string.charAt(i));
-				count=1;
-			}
-		}
-		return temp;
+				if(count>1) {
+				temp.append(count);
+				}				
+				count=0;
+			}			
+		}		
+        string=temp;
+        System.out.println(string);
+        
 	}
-	public static void main(String[] args) {
+	}
 		
-		System.out.println(RLEPrint("hhhhhhhhhhhhggghhhhhhhhhhh"));
-	}
-
 }
