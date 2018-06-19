b100b848f2534bfd5b4df4a1a5d2b836647b21a6
Zinko Victor <zinkoexp@gmail.com>
2018-06-19 09:58:53 +0300

    ff

--- Prometheus/fibonacci/Fibonacci.java
+++ Prometheus/fibonacci/Fibonacci.java
@@ -3,10 +3,11 @@ package fibonacci;
 public class Fibonacci {
 		
 		
-    public static long getNumber(int position){
-    	if (position == 1||position==2) return 1;
-        else if (position>2) return getNumber(position-1)+getNumber(position-2);
-        else return -1;
+    public static long getNumber(int position){    	
+    		if (position == 1||position==2) return 1;
+            else if (position>2) return getNumber(position-1)+getNumber(position-2);
+            else return -1;   	
+    	
     }
 }
 
--- Prometheus/fibonacci/run.java
+++ Prometheus/fibonacci/run.java
@@ -3,7 +3,15 @@ package fibonacci;
 public class run {
 
 	public static void main(String[] args) {
-		System.out.println(Fibonacci.getNumber(9));
+		int a = 1;
+		int b = 12;
+
+		for(int i = a; i<=b;i++){
+			long rez = Fibonacci.getNumber(i);
+			if ((rez%2)==0) {
+		System.out.println(rez);
+			}
+		}		
 
 	}
 
